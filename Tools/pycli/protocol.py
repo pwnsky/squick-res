bd00db606f26c9c04700c9cc877068f076a41d15
FRONTIER-GP-徐绿国 <l418894113@gmail.com>
2026-01-17 17:47:42 +0800

    Update: Remove header size

--- Tools/pycli/protocol.py
+++ Tools/pycli/protocol.py
@@ -15,13 +15,17 @@ from google.protobuf.json_format import MessageToJson
 print("sys", sys.path)
 
 def Encode(msg_id, data):
-    send_data = b'' 
-    send_data += (msg_id).to_bytes(2, byteorder="big", signed=False)
-    send_data += (len(data) + 6).to_bytes(4, byteorder="big", signed=False)
+    send_data = b'S' 
+    send_data += (msg_id).to_bytes(4, byteorder="big", signed=False)
+    send_data += len(data).to_bytes(4, byteorder="big", signed=False)
     send_data += data
+    print("send_data: ", send_data)
     return send_data
 
 def Decode(recv_data):
-    msg_id = int.from_bytes(recv_data[0:2], byteorder='big', signed=False)
-    length = int.from_bytes(recv_data[2:6], byteorder='big', signed=False)
-    return msg_id, recv_data[6:]
+    msg_signature = recv_data[0:1]
+    if msg_signature != b'S':
+        print("Error: invalid msg signature")
+    msg_id = int.from_bytes(recv_data[1:5], byteorder='big', signed=False)
+    length = int.from_bytes(recv_data[5:9], byteorder='big', signed=False)
+    return msg_id, recv_data[9:], length
